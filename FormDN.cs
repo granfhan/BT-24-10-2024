e452fd4a2170c5224b3805bbace44517aaa7da6f
granfhan <121719521+granfhan@users.noreply.github.com>
2024-10-24 15:39:37 +0700

    Add files via upload

---
+++ FormDN.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormDangNhap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DNvalid(string dn)
+        {
+        }
+        private void Passvalid(string pass)
+        {
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (DN.Text == "admin" && Pass.Text == "1234")
+            {
+                MessageBox.Show("Đăng nhập thành công");
+            }
+            else
+            {
+                MessageBox.Show("Tài khoản hoặc mật khẩu sai");
+            }    
+        }
+        
+
+        private void DN_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Pass_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ QLSV.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLySinhVien
+{
+    public partial class Form1 : Form
+    {
+
+        public class SinhVien
+        {
+            public string MaSinhVien { get; set; }
+            public string HoTen { get; set; }
+            public string LopHoc { get; set; }
+        } 
+
+        private List<SinhVien> danhSachSinhVien = new List<SinhVien>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void HienThiDanhSach()
+        {
+            dataGridviewSV.DataSource = null;
+            dataGridviewSV.DataSource = danhSachSinhVien;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SinhVien sv = new SinhVien
+            {
+                MaSinhVien = mssv.Text,
+                HoTen = tensv.Text,
+                LopHoc = LopSV.Text
+            };
+            danhSachSinhVien.Add(sv);
+            HienThiDanhSach();
+            xoa();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridviewSV.CurrentRow != null)
+            {
+                mssv.Text = dataGridviewSV.CurrentRow.Cells[0].Value.ToString();
+                tensv.Text = dataGridviewSV.CurrentRow.Cells[1].Value.ToString();
+                LopSV.Text = dataGridviewSV.CurrentRow.Cells[2].Value.ToString();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void xoa()
+        {
+            mssv.Clear();
+            tensv.Clear();
+            LopSV.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridviewSV.CurrentRow != null)
+            {
+                int index = dataGridviewSV.CurrentRow.Index;
+                danhSachSinhVien[index].MaSinhVien = mssv.Text;
+                danhSachSinhVien[index].HoTen = tensv.Text;
+                danhSachSinhVien[index].LopHoc = LopSV.Text;
+                HienThiDanhSach();
+                xoa();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (dataGridviewSV.CurrentRow != null)
+            {
+                int index = dataGridviewSV.CurrentRow.Index;
+                danhSachSinhVien.RemoveAt(index);
+                HienThiDanhSach();
+                xoa();
+            }
+        }
+    }
+}
